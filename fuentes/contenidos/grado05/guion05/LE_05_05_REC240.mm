b9082e5bce8c61a9a6e55affb29db5fdae9e6101
kosmosdelfico <kosmosdelfico@gmail.com>
2015-03-10 16:40:45 -0500

    Docs autor

---
+++ fuentes/contenidos/grado05/guion05/LE_05_05_REC240.mm
@@ -0,0 +1,57 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cccccc" CREATED="1425903006667" ID="ID_1627332700" MODIFIED="1425905894730" TEXT="VERBO">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425903025933" HGAP="-8" ID="ID_47373502" MODIFIED="1425905865694" POSITION="right" TEXT="Modo" VSHIFT="-19">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425903589941" ID="ID_1306523762" MODIFIED="1425905163158" TEXT="Indicativo: Expresa algo que se da como real, que es, fue o ser&#xe1;"/>
+<node CREATED="1425903608709" ID="ID_1468362032" MODIFIED="1425905151156" TEXT="Subjuntivo: Expresa deseos, posibilidades o probabilidades"/>
+<node CREATED="1425903968631" ID="ID_1389945698" MODIFIED="1425904015620" TEXT="Imperativo: Expresa &#xf3;rdenes o prohibiciones que deben cumplirse"/>
+</node>
+<node CREATED="1425903045022" HGAP="6" ID="ID_478200787" MODIFIED="1425905863760" POSITION="right" TEXT="Formas impersonales" VSHIFT="-42">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425903256100" ID="ID_626142425" MODIFIED="1425903272168" TEXT="Infinitivo: Termina en -ar, -er, -ir"/>
+<node CREATED="1425903275468" ID="ID_625473681" MODIFIED="1425903299889" TEXT="Gerundio: Termina en -ando, -iendo, -yendo"/>
+<node CREATED="1425903311476" ID="ID_703756009" MODIFIED="1425903342460" TEXT="Participio: Termina en -ado, -ido"/>
+</node>
+<node CREATED="1425905687119" HGAP="10" ID="ID_942430751" MODIFIED="1425905867603" POSITION="left" TEXT="Expresa" VSHIFT="-25">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425905696337" ID="ID_1908839966" MODIFIED="1425905700488" TEXT="Acciones"/>
+<node CREATED="1425905712782" ID="ID_951012109" MODIFIED="1425905718536" TEXT="Estados"/>
+<node CREATED="1425905720855" ID="ID_298279214" MODIFIED="1425905728513" TEXT="Ocurrencia de fen&#xf3;menos"/>
+</node>
+<node CREATED="1425904503140" HGAP="9" ID="ID_980611932" MODIFIED="1425905869638" POSITION="left" TEXT="Elementos" VSHIFT="-66">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425904510869" ID="ID_462928790" MODIFIED="1425904531646" TEXT="Lexema o ra&#xed;z"/>
+<node CREATED="1425904549874" ID="ID_581566993" MODIFIED="1425904563942" TEXT="Desinencia o terminaci&#xf3;n"/>
+</node>
+<node CREATED="1425903675485" HGAP="26" ID="ID_221263931" MODIFIED="1425905855576" POSITION="left" TEXT="Tipos" VSHIFT="-19">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425903680650" ID="ID_548424866" MODIFIED="1425905303092" TEXT="Regulares: Conjugaci&#xf3;n uniforme, no se modifica el lexema"/>
+<node CREATED="1425903739582" ID="ID_484980821" MODIFIED="1425905290491" TEXT="Irregulares: Se modifica el lexema en ciertos modos y tiempos"/>
+</node>
+<node CREATED="1425903835197" HGAP="17" ID="ID_1201205477" MODIFIED="1425905857593" POSITION="left" TEXT="Clasificaci&#xf3;n" VSHIFT="65">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425903849479" ID="ID_212336335" MODIFIED="1425905921033" TEXT="-ar: Primera conjugaci&#xf3;n">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1425903862516" ID="ID_293258057" MODIFIED="1425903873908" TEXT="-er: Segunda conjugaci&#xf3;n"/>
+<node CREATED="1425903877051" ID="ID_399557369" MODIFIED="1425903886181" TEXT="-ir: Tercera conjugaci&#xf3;n"/>
+</node>
+<node CREATED="1425904495685" ID="ID_1318238360" MODIFIED="1425905861832" POSITION="right" TEXT="Tiempos simples" VSHIFT="2">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425904978038" ID="ID_1447461270" MODIFIED="1425905100564" TEXT="Presente: que ocurre ahora"/>
+<node CREATED="1425905049171" ID="ID_576159673" MODIFIED="1425905115221" TEXT="Pret&#xe9;rito: que ya ocurri&#xf3;"/>
+<node CREATED="1425905118027" ID="ID_1222325616" MODIFIED="1425905130684" TEXT="Futuro: que ocurrir&#xe1;"/>
+</node>
+<node CREATED="1425905377016" HGAP="-3" ID="ID_764782096" MODIFIED="1425905859635" POSITION="right" TEXT="Persona gramatical" VSHIFT="39">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425905399132" ID="ID_1825468919" MODIFIED="1425905561113" TEXT="Primera singular:Yo"/>
+<node CREATED="1425905427167" ID="ID_256329262" MODIFIED="1425905574291" TEXT="Segunda singular:T&#xfa;, usted, vos"/>
+<node CREATED="1425905460321" ID="ID_293146900" MODIFIED="1425905584209" TEXT="Tercera singular &#xc9;l, ella"/>
+<node CREATED="1425905550549" ID="ID_1978247035" MODIFIED="1425905620696" TEXT="Primera plural: Nosotros, nosotras"/>
+<node CREATED="1425905552162" ID="ID_1445739797" MODIFIED="1425905645999" TEXT="Segunda plural: Ustedes, vosotros, vosotras"/>
+<node CREATED="1425905554022" ID="ID_420327976" MODIFIED="1425905658144" TEXT="Tercera plural: Ellos, ellas"/>
+</node>
+</node>
+</map>
