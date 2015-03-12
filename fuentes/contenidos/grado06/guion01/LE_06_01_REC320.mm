3e75eec2a7e2d8fc89e23efe40277629948d79d2
cbpinedat <cristianpinedatriana@gmail.com>
2015-03-11 23:42:56 -0500

    Versión editada
    Archivos corregidos y editados. Se eliminaron los archivos de recursos
    que no debían estar aparte por ser recursos aprovechados.

--- fuentes/contenidos/grado06/guion01/LE_06_01_REC320.mm
+++ fuentes/contenidos/grado06/guion01/LE_06_01_REC320.mm
@@ -23,9 +23,9 @@
 <node CREATED="1424738854728" ID="ID_1644606150" MODIFIED="1424740121852" STYLE="bubble" TEXT="G&#xe9;nero l&#xed;rico">
 <node CREATED="1424739091669" HGAP="15" ID="ID_50855393" MODIFIED="1424740121852" STYLE="bubble" TEXT="Son textos que expresan&#xa; los sentimientos del autor.&#xa;Suelen escribirse en verso &#xa;y pueden tener rima." VSHIFT="27">
 <node CREATED="1424739633015" HGAP="15" ID="ID_1513316415" MODIFIED="1424740121852" STYLE="bubble" TEXT="Posee">
-<node CREATED="1424739664027" ID="ID_1010459528" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Evoca sentimientos o impresiones del yo l&#xed;rico o po&#xe9;tico."/>
-<node CREATED="1424739664027" ID="ID_619220794" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        No hay desarrollo de acciones."/>
-<node CREATED="1424739664027" ID="ID_1403742330" MODIFIED="1424748800207" STYLE="bubble" TEXT="&#x2022;        Posee una estructura de  versos y estrofas."/>
+<node CREATED="1424739664027" ID="ID_1010459528" MODIFIED="1426134571018" STYLE="bubble" TEXT="&#x2022;        Sentimientos o impresiones del yo l&#xed;rico o po&#xe9;tico."/>
+<node CREATED="1424739664027" ID="ID_619220794" MODIFIED="1426134588733" STYLE="bubble" TEXT="&#x2022;        Poco o ning&#xfa;n desarrollo de acciones."/>
+<node CREATED="1424739664027" ID="ID_1403742330" MODIFIED="1426134608910" STYLE="bubble" TEXT="&#x2022;        Una estructura de versos y estrofas."/>
 </node>
 </node>
 </node>
@@ -33,8 +33,8 @@
 <node CREATED="1424739169939" HGAP="15" ID="ID_1812335263" MODIFIED="1424740121852" STYLE="bubble" TEXT="Son obras en forma &#xa;de di&#xe1;logo que se&#xa; crean para ser &#xa;representadas &#xa;en el teatro." VSHIFT="9">
 <node CREATED="1424739824221" HGAP="15" ID="ID_356847966" MODIFIED="1424740121852" STYLE="bubble" TEXT="Posee" VSHIFT="2">
 <node CREATED="1424739827239" ID="ID_927811749" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Di&#xe1;logos, mon&#xf3;logos y acciones de personajes."/>
-<node CREATED="1424739827239" ID="ID_1130863290" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Objetivo de ser representada por actores."/>
-<node CREATED="1424739827239" ID="ID_1817318452" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Se estructura en escenas y actos."/>
+<node CREATED="1424739827239" ID="ID_1130863290" MODIFIED="1426134634583" STYLE="bubble" TEXT="&#x2022;        El objetivo de ser representada por actores."/>
+<node CREATED="1424739827239" ID="ID_1817318452" MODIFIED="1426134643319" STYLE="bubble" TEXT="&#x2022;        Estructura con escenas y actos."/>
 </node>
 </node>
 </node>
@@ -47,7 +47,7 @@
 <node CREATED="1424738626711" HGAP="18" ID="ID_1423083344" MODIFIED="1424740121852" STYLE="bubble" TEXT="Conjunto de palabras &#xa;que forman una unidad &#xa;m&#xe9;trica dentro de una &#xa;estrofa po&#xe9;tica.  " VSHIFT="1"/>
 </node>
 <node CREATED="1424738072768" ID="ID_1941002893" MODIFIED="1424740121852" STYLE="bubble" TEXT="Prosa">
-<node CREATED="1424738541265" HGAP="18" ID="ID_63643596" MODIFIED="1424740121852" STYLE="bubble" TEXT="La prosa es la forma &#xa;natural del lenguaje &#xa;para expresar  ideas &#xa;o conceptos." VSHIFT="24"/>
+<node CREATED="1424738541265" HGAP="18" ID="ID_63643596" MODIFIED="1426134667573" STYLE="bubble" TEXT="Es la forma &#xa;natural del lenguaje &#xa;para expresar  ideas &#xa;o conceptos." VSHIFT="24"/>
 </node>
 </node>
 <node CREATED="1424737771050" HGAP="14" ID="ID_99170034" MODIFIED="1424740121852" STYLE="bubble" TEXT="Combinaciones textuales" VSHIFT="25">
@@ -55,10 +55,10 @@
 <node CREATED="1424738396601" HGAP="15" ID="ID_1703648611" MODIFIED="1424740121852" STYLE="bubble" TEXT="Realiza representaciones &#xa;ling&#xfc;&#xed;sticas del mundo real &#xa;o ficticio y aquello que &#xa;lo compone." VSHIFT="-10"/>
 </node>
 <node CREATED="1424738024303" HGAP="29" ID="ID_951642262" MODIFIED="1424740121852" STYLE="bubble" TEXT="El di&#xe1;logo" VSHIFT="-10">
-<node CREATED="1424738340287" ID="ID_485227739" MODIFIED="1424740121852" STYLE="bubble" TEXT="Forma del discurso oral y escrito &#xa;en la que se comunican&#xa;entre s&#xed; dos o m&#xe1;s personas&#xa; interlocutores), &#xa;en un intercambio de ideas"/>
+<node CREATED="1424738340287" ID="ID_485227739" MODIFIED="1426134691593" STYLE="bubble" TEXT="Forma del discurso oral y escrito &#xa;en la que se comunican&#xa;entre s&#xed; dos o m&#xe1;s personas&#xa;(interlocutores);&#xa;en un intercambio de ideas"/>
 </node>
 <node CREATED="1424738004025" HGAP="16" ID="ID_1733776919" MODIFIED="1424740121852" STYLE="bubble" TEXT="La narraci&#xf3;n" VSHIFT="6">
-<node CREATED="1424738139452" ID="ID_1253175934" MODIFIED="1424740121852" STYLE="bubble" TEXT="Tiene el objetivo de exponer hechos, &#xa;sucesos, historias que les suceden&#xa; a unos personajes en un determinado&#xa; tiempo y espacio"/>
+<node CREATED="1424738139452" ID="ID_1253175934" MODIFIED="1426134703497" STYLE="bubble" TEXT="Tiene el objetivo de exponer hechos, &#xa;sucesos, historias que les suceden&#xa; a unos personajes en un determinado&#xa; tiempo y espacio."/>
 </node>
 </node>
 </node>
