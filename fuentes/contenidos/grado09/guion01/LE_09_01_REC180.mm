531d017fa625320edd1f865f93b3e591416e5f28
Marcofi <esprue@hotmail.com>
2015-03-13 11:02:05 -0500

    Tema1 grado 9
    Corrección tema 1 grado 9 en la carpeta correspondiente. Eliminé el
    archivo comprimido porque lo había incluido en una carpeta equivocada.

---
+++ fuentes/contenidos/grado09/guion01/LE_09_01_REC180.mm
@@ -0,0 +1,348 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1426005455287" ID="ID_1199703086" MODIFIED="1426005489704" TEXT="La literatura precolombina, de la Conquista y la Colonia">
+<node CREATED="1426005494046" ID="ID_1166586854" MODIFIED="1426013420717" POSITION="right" TEXT="se divide en dos momentos">
+<icon BUILTIN="idea"/>
+<node CREATED="1426005570559" HGAP="31" ID="ID_1052972197" MODIFIED="1426012664261" TEXT="Literatura de la Conquista y la Colonia" VSHIFT="48">
+<node CREATED="1426009504096" HGAP="40" ID="ID_725954693" MODIFIED="1426012498661" TEXT="se desarroll&#xf3; durante" VSHIFT="-97">
+<icon BUILTIN="idea"/>
+<node CREATED="1426009522343" ID="ID_1360959283" MODIFIED="1426009550080" TEXT="Inicios de siglo XVI hasta inicios del XIX"/>
+</node>
+<node CREATED="1426009573324" ID="ID_1022685976" MODIFIED="1426012505746" TEXT="sus g&#xe9;neros fueron">
+<icon BUILTIN="idea"/>
+<node CREATED="1426009609673" HGAP="21" ID="ID_1402695273" MODIFIED="1426009688788" TEXT="El narrativo" VSHIFT="-18">
+<node CREATED="1426009698169" ID="ID_1373594025" MODIFIED="1426012492894" TEXT="dividido en">
+<icon BUILTIN="idea"/>
+<node CREATED="1426009749144" ID="ID_1924048699" MODIFIED="1426009758803" TEXT="Cr&#xf3;nicas de indias">
+<node CREATED="1426011348008" ID="ID_673263305" MODIFIED="1426012489123" TEXT="clasificadas seg&#xfa;n el origen de sus autores en">
+<icon BUILTIN="idea"/>
+<node CREATED="1426011372704" ID="ID_1997413721" MODIFIED="1426011390119" TEXT="Cr&#xf3;nicas espa&#xf1;olas">
+<node CREATED="1426011789216" ID="ID_972795825" MODIFIED="1426013237726" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1426011801216" HGAP="26" ID="ID_1675983793" MODIFIED="1426013265335" VSHIFT="-54">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        <i>Historia verdadera de la conquista de la Nueva Espa&#241;a</i>
+      </li>
+      <li>
+        <i>Brev&#237;sima relaci&#243;n de la destrucci&#243;n de las Indias</i>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1426011375169" ID="ID_1688549910" MODIFIED="1426011399702" TEXT="Cr&#xf3;nicas mestizas">
+<node CREATED="1426011869168" ID="ID_1917023143" MODIFIED="1426013240201" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1426011877970" ID="ID_925631801" MODIFIED="1426012075863">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <i>Comentarios Reales de los Incas</i>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1426011379704" ID="ID_1403361067" MODIFIED="1426011409341" TEXT="Cr&#xf3;nicas ind&#xed;genas">
+<node CREATED="1426012082602" ID="ID_1471577444" MODIFIED="1426013242781" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1426012089202" ID="ID_884783897" MODIFIED="1426012114593">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <i>Nueva cor&#243;nica y buen gobierno</i>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426009762407" ID="ID_1370492077" MODIFIED="1426011339295" TEXT="Otras narraciones">
+<node CREATED="1426011433409" ID="ID_553694994" MODIFIED="1426012483429" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1426011440409" ID="ID_1700687801" MODIFIED="1426011499297">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      La novela <i>El desierto prodigioso y prodigio del desierto</i>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426009637914" ID="ID_1068558103" MODIFIED="1426009665732" TEXT="El po&#xe9;tico">
+<node CREATED="1426011523465" ID="ID_607431199" MODIFIED="1426012474940" TEXT="que fue de corte">
+<icon BUILTIN="idea"/>
+<node CREATED="1426011541840" ID="ID_1929999203" MODIFIED="1426011556147" TEXT="Barroco">
+<node CREATED="1426011570361" ID="ID_1479161481" MODIFIED="1426012479876" TEXT="caracterizado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1426011583323" ID="ID_942672351" MODIFIED="1426011703618">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        El uso de la paradoja y la met&#225;fora
+      </li>
+      <li>
+        La alteraci&#243;n de la sintaxis
+      </li>
+      <li>
+        La exaltaci&#243;n de la inteligencia
+      </li>
+      <li>
+        El esteticismo
+      </li>
+      <li>
+        El t&#243;pico llamado <i>memento mori</i>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426011557824" ID="ID_1748014659" MODIFIED="1426012471900" TEXT="cuyos autores m&#xe1;s destacados fueron">
+<icon BUILTIN="idea"/>
+<node CREATED="1426011732617" ID="ID_95749301" MODIFIED="1426011750824" TEXT="El sat&#xed;rico peruano Juan del Valle y Caviedes"/>
+<node CREATED="1426011736368" ID="ID_826183992" MODIFIED="1426011772750" TEXT="La mexicana sor Juana In&#xe9;s de la Cruz"/>
+</node>
+</node>
+<node CREATED="1426009643304" ID="ID_1929170814" MODIFIED="1426009690836" TEXT="El teatral" VSHIFT="28">
+<node CREATED="1426012142849" ID="ID_384894264" MODIFIED="1426012462465" TEXT="que fue una">
+<icon BUILTIN="idea"/>
+<node CREATED="1426012355950" ID="ID_844868290" MODIFIED="1426012366555" TEXT="Herramienta de evangelizaci&#xf3;n"/>
+</node>
+<node CREATED="1426012158753" ID="ID_1301397981" MODIFIED="1426013297321" TEXT="cuyos principales autores fueron">
+<icon BUILTIN="idea"/>
+<node CREATED="1426012397472" ID="ID_808362431" MODIFIED="1426012443124">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sor Juana In&#233;s de la Cruz
+      </li>
+      <li>
+        Juan Ruiz de Alarc&#243;n
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426005595527" HGAP="41" ID="ID_836667033" MODIFIED="1426012674045" TEXT="Literatura precolombina" VSHIFT="302">
+<node CREATED="1426006641733" HGAP="10" ID="ID_495377885" MODIFIED="1426008200652" TEXT="se produjo en tres grandes culturas" VSHIFT="-102">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007018295" HGAP="16" ID="ID_1800187879" MODIFIED="1426007461588" TEXT="Maya (1000 a. C.-1697)" VSHIFT="-47">
+<node CREATED="1426007490832" ID="ID_460504707" MODIFIED="1426008583750" TEXT="cuyos g&#xe9;neros principales fueron">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007519192" ID="ID_1882865323" MODIFIED="1426007537549" TEXT="El narrativo">
+<node CREATED="1426007552528" ID="ID_562289169" MODIFIED="1426008544866" TEXT="con obras como">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007577464" ID="ID_596227776" MODIFIED="1426007661314">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        El <i>Popol Vuh</i>
+      </li>
+      <li>
+        El <i>Chilam Balam</i>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1426007524016" HGAP="22" ID="ID_1386798702" MODIFIED="1426007685572" TEXT="El teatral" VSHIFT="6">
+<node CREATED="1426007677960" ID="ID_420633086" MODIFIED="1426008550035" TEXT="cuya obra m&#xe1;s representativa fue">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007871048" ID="ID_690587188" MODIFIED="1426007897683">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      El <i>Rabinal Ach&#237;</i>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426007022438" ID="ID_659674894" MODIFIED="1426007102436" TEXT="Azteca (1300-1521)">
+<node CREATED="1426007935024" ID="ID_1795635230" MODIFIED="1426008578295" TEXT="cuyos g&#xe9;nero principales fueron">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007956383" ID="ID_100067700" MODIFIED="1426007974321" TEXT="El narrativo">
+<node CREATED="1426008034552" ID="ID_608024798" MODIFIED="1426008554921" TEXT="expresado en">
+<icon BUILTIN="idea"/>
+<node CREATED="1426008049376" HGAP="22" ID="ID_1560306027" MODIFIED="1426008148715" VSHIFT="1">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Mitos
+      </li>
+      <li>
+        Leyendas
+      </li>
+      <li>
+        C&#243;dices de corte hist&#243;rico
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1426007981176" ID="ID_346891091" MODIFIED="1426007998990" TEXT="El po&#xe9;tico">
+<node CREATED="1426008132176" HGAP="26" ID="ID_641442837" MODIFIED="1426008560172" TEXT="ligado a" VSHIFT="10">
+<icon BUILTIN="idea"/>
+<node CREATED="1426008294632" ID="ID_894122960" MODIFIED="1426008334484" TEXT="Lo divino, con el dios Xochipilli"/>
+</node>
+<node CREATED="1426008337183" ID="ID_1485400219" MODIFIED="1426008565050" TEXT="expresado en">
+<icon BUILTIN="idea"/>
+<node CREATED="1426008386088" ID="ID_1281130210" MODIFIED="1426008411719" TEXT="La tradici&#xf3;n in Xochitl in Cu&#xed;catl"/>
+</node>
+<node CREATED="1426008415216" ID="ID_1011804881" MODIFIED="1426008573373" TEXT="cuyos autores fueron">
+<icon BUILTIN="idea"/>
+<node CREATED="1426008440480" ID="ID_521858394" MODIFIED="1426008473317" TEXT="Pr&#xed;ncipes y sacerdotes"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426007026256" ID="ID_407175298" MODIFIED="1426007474268" TEXT="Inca (1200-1532)" VSHIFT="26">
+<node CREATED="1426008513072" ID="ID_719419293" MODIFIED="1426009420316" TEXT="caracterizada por">
+<icon BUILTIN="idea"/>
+<node CREATED="1426008638768" ID="ID_407869847" MODIFIED="1426009376325" TEXT="Estar ligada exclusivamente con la tradici&#xf3;n oral"/>
+</node>
+<node CREATED="1426008667113" ID="ID_1542665053" MODIFIED="1426009406060" TEXT="cuyos g&#xe9;neros principales fueron">
+<icon BUILTIN="idea"/>
+<node CREATED="1426008692999" ID="ID_1923053217" MODIFIED="1426008711278" TEXT="El narrativo">
+<node CREATED="1426009051360" ID="ID_68471011" MODIFIED="1426013183117" TEXT="representado en">
+<icon BUILTIN="idea"/>
+<node CREATED="1426009061968" ID="ID_731313704" MODIFIED="1426009239142">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Mitos
+      </li>
+      <li>
+        Leyendas
+      </li>
+      <li>
+        El <i>Manuscrito de Huarochir&#237;</i>
+      </li>
+      <li>
+        Fragmentos de cr&#243;nicas de Indias
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1426008713320" ID="ID_1661142372" MODIFIED="1426008727357" TEXT="El po&#xe9;tico">
+<node CREATED="1426008730800" ID="ID_926939352" MODIFIED="1426009394185" TEXT="que tuvo dos vertientes">
+<icon BUILTIN="idea"/>
+<node CREATED="1426008742576" ID="ID_42805999" MODIFIED="1426013200028">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      La oficial, producida por los <i>amautas</i>&#160;como <i>Jaillis</i>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1426008746152" ID="ID_1709119407" MODIFIED="1426013206849">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      La popular, producida por los <i>haravicus</i>&#160;como <i>arawis</i>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426006650697" ID="ID_965557024" MODIFIED="1426007416450" TEXT="que se define como">
+<icon BUILTIN="idea"/>
+<node CREATED="1426006801630" ID="ID_148281999" MODIFIED="1426006897414" TEXT="Las manifestaciones literarias que se produjeron en este lado del mundo antes de la llegada de los espa&#xf1;oles."/>
+</node>
+<node CREATED="1426006756782" HGAP="24" ID="ID_1512040087" MODIFIED="1426007399791" TEXT="proviene de" VSHIFT="118">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007147686" ID="ID_639706890" MODIFIED="1426007158501" TEXT="La tradici&#xf3;n oral">
+<node CREATED="1426007162254" ID="ID_1441114383" MODIFIED="1426007406708" TEXT="relacionada con">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007326319" ID="ID_857394817" MODIFIED="1426007388962" TEXT="La religi&#xf3;n, los mitos y las pr&#xe1;cticas sociales."/>
+</node>
+<node CREATED="1426007166183" ID="ID_1798287106" MODIFIED="1426007403996" TEXT="que es">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007197030" HGAP="34" ID="ID_283807687" MODIFIED="1426007243202" TEXT="La transmisi&#xf3;n de creencias y conocimientos de generaci&#xf3;n en generaci&#xf3;n" VSHIFT="26">
+<node CREATED="1426007246078" ID="ID_252665774" MODIFIED="1426007409887" TEXT="por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1426007254079" ID="ID_1869020082" MODIFIED="1426007323415" TEXT="La palabra, a la que se le atribuy&#xf3; poder propio."/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
