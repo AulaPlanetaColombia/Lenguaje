1f8e95014bdc50a2aede355df6d95f527c5f0c22
Gabriel Rojas Andrade <gabriel0000@gmail.com>
2015-03-26 15:52:49 -0500

    Documentos de autor LE_07_02_CO y LE_10_02_CO. Edición LE_10_01_CO

---
+++ fuentes/contenidos/grado07/guion02/LE_07_02_REC200.mm
@@ -0,0 +1,78 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425970016095" ID="ID_1763574919" MODIFIED="1425971755326" TEXT="El G&#xe9;nero Narrativo">
+<font NAME="Times New Roman" SIZE="24"/>
+<node CREATED="1425970082210" HGAP="-44" ID="ID_577942266" MODIFIED="1425972234471" POSITION="right" TEXT="&#xbf;Qu&#xe9; es?" VSHIFT="348">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1425970096119" HGAP="10" ID="ID_1022538096" MODIFIED="1425970520129" TEXT="" VSHIFT="-56">
+<node CREATED="1425970413404" ID="ID_187661740" MODIFIED="1425971304870">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="12.0pt" face="Times New Roman">&#160;Es un g&#233;nero literario que relata una historia, ya sea real o ficticia. Dependiendo de la historia que se relata existen varios tipos de narrador as&#237; como de subg&#233;neros narrativos.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1425970543700" HGAP="-85" ID="ID_1267134443" MODIFIED="1425971982090" POSITION="left" TEXT="&#xbf;Qui&#xe9;n relata la historia?" VSHIFT="-156">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1425970594182" HGAP="12" ID="ID_263222140" MODIFIED="1425971310686" TEXT="El narrador" VSHIFT="31">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1425970626962" HGAP="12" ID="ID_779458360" MODIFIED="1425971493827" TEXT="Puede ser" VSHIFT="24">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1425970677044" HGAP="10" ID="ID_1906163097" MODIFIED="1425971522731" TEXT="Interno o en 1a persona" VSHIFT="93">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1425970710512" HGAP="17" ID="ID_1962404293" MODIFIED="1425971324919" TEXT="Protagonista" VSHIFT="-59">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1425970823979" HGAP="17" ID="ID_1656776658" MODIFIED="1425971329253" TEXT="La historia gira en torno a &#xe9;l" VSHIFT="-28">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1425970723740" HGAP="12" ID="ID_765074135" MODIFIED="1425971319985" TEXT="Personaje secundario" VSHIFT="43">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1425970891501" HGAP="12" ID="ID_585619960" MODIFIED="1425971548562" TEXT="Es parte de la historia pero no el pesonaje m&#xe1;s importante" VSHIFT="-40">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1425970693919" HGAP="14" ID="ID_947264868" MODIFIED="1425971540286" TEXT="Externo o en 3a persona" VSHIFT="-45">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1425970734735" HGAP="11" ID="ID_417460099" MODIFIED="1425971342633" TEXT="Omnisciente" VSHIFT="-87">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1425970933967" HGAP="10" ID="ID_1242208829" MODIFIED="1425971561376" TEXT="Sabe todo de todos los personajes y de la historia, incluso los pensamientos" VSHIFT="31">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1425970744032" HGAP="11" ID="ID_791861462" MODIFIED="1425971346717" TEXT="Observador" VSHIFT="60">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1425971080637" HGAP="16" ID="ID_1292024677" MODIFIED="1425971354783" TEXT="Conoce la historia desde una posici&#xf3;n lejana" VSHIFT="21">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425971254927" HGAP="-65" ID="ID_458760336" MODIFIED="1425972244079" POSITION="right" TEXT="Subg&#xe9;neros narrativos" VSHIFT="-164">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1425971372770" HGAP="-6" ID="ID_1230627284" MODIFIED="1425972100131" TEXT="Pueden ser" VSHIFT="74">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1425971403120" HGAP="-18" ID="ID_460923033" MODIFIED="1425972274956" TEXT="Literarios" VSHIFT="302">
+<font NAME="Times New Roman" SIZE="18"/>
+<node CREATED="1425971606924" HGAP="24" ID="ID_1011974244" MODIFIED="1425972197762" TEXT="Cuento, relato, novela, mito leyenda, f&#xe1;bula, saga, " VSHIFT="-40"/>
+</node>
+<node CREATED="1425971417750" HGAP="152" ID="ID_1213050005" MODIFIED="1425972267856" TEXT="No literarios" VSHIFT="-404">
+<font NAME="Times New Roman" SIZE="18"/>
+<node CREATED="1425971585744" ID="ID_170384853" MODIFIED="1425971596562" TEXT="Carta, peri&#xf3;dico, diario"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ fuentes/contenidos/grado10/guion02/LE_10_02_REC170.mm
@@ -0,0 +1,104 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425776128699" ID="ID_518590687" MODIFIED="1425776141192" TEXT="La Literatura Barroca">
+<node CREATED="1425776148229" ID="ID_107882710" MODIFIED="1425777717816" POSITION="right" TEXT="Literatura">
+<icon BUILTIN="idea"/>
+<node CREATED="1425777774420" ID="ID_764943878" MODIFIED="1425777790487" TEXT="Poes&#xed;a">
+<icon BUILTIN="idea"/>
+<node CREATED="1425777826816" ID="ID_1492702772" MODIFIED="1425777833172" TEXT="De tradici&#xf3;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1425777843699" ID="ID_103561925" MODIFIED="1425777855499" TEXT="Popular">
+<icon BUILTIN="idea"/>
+<node CREATED="1425777877875" ID="ID_1539405342" MODIFIED="1425777884673" TEXT="Puede ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1425777893023" ID="ID_1388640671" MODIFIED="1425777945704" TEXT="L&#xed;rica: villancico, letrilla, copla, etc. Narrativa: romancero nuevo.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+<node CREATED="1425777858265" ID="ID_468073932" MODIFIED="1425777862720" TEXT="Culta">
+<icon BUILTIN="idea"/>
+<node CREATED="1425777951961" ID="ID_1047986114" MODIFIED="1425777961253" TEXT="Puede ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1425777964234" ID="ID_866892007" MODIFIED="1425778379712" TEXT="L&#xed;rica: *estilo luterano: Luis de G&#xf3;ngora, Sor Juana In&#xe9;s de la Cruz. *Estilo conceptista: Francisco de Quevedo. *Estilo clasicista: F&#xe9;lix Lope de Vega. *Principal tipo de poema: soneto. *Ep&#xed;stola, oda, etc.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425777793443" ID="ID_987018278" MODIFIED="1425777800304" TEXT="Prosa">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778383859" ID="ID_1755057124" MODIFIED="1425778399871" TEXT="Puede ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778403199" ID="ID_1100694682" MODIFIED="1425778495774" TEXT="*Novela de tema moral, did&#xe1;ctico, filos&#xf3;fico, pol&#xed;tico, etc. El Critic&#xf3;n, Baltasar Graci&#xe1;n. *Novela picaresca Guzm&#xe1;n de Alfarache, Mateo Alem&#xe1;n. El Busc&#xf3;n, Francisco de Quevedo.">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1425778555047" ID="ID_21262531" MODIFIED="1425778564364" TEXT="Se destaca">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778567143" ID="ID_921082090" MODIFIED="1425778596276" TEXT="Fin de la novela de caballer&#xed;as. El Quijote, Miguel de Cervantes.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425777804218" ID="ID_611618300" MODIFIED="1425777813628" TEXT="Teatro">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778599184" ID="ID_675145426" MODIFIED="1425778604972" TEXT="Se crea">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778607751" ID="ID_1894737607" MODIFIED="1425778616698" TEXT="La comedia nueva">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778621577" ID="ID_523931357" MODIFIED="1425778635289" TEXT="Impulsada por">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778639362" ID="ID_1605629879" MODIFIED="1425778658467" TEXT="F&#xe9;lix Lope de Vega, Tirso Molina y Pedro Calder&#xf3;n de la Barca.">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778667045" ID="ID_1140347870" MODIFIED="1425778674142" TEXT="Puede ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778676995" ID="ID_339649065" MODIFIED="1425778685919" TEXT="Comedia">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1425778688497" ID="ID_286646937" MODIFIED="1425778708742" TEXT="Drama">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+<node CREATED="1425778713123" ID="ID_623698286" MODIFIED="1425778728563" TEXT=""/>
+</node>
+<node CREATED="1425778740296" ID="ID_765011626" MODIFIED="1425778750125" TEXT="Con influencias de">
+<icon BUILTIN="idea"/>
+<node CREATED="1425778757373" ID="ID_684782353" MODIFIED="1425778816817" TEXT="*Commedia dell&apos;arte. *Teatro isabelino (William Shakespeare). *Comedia francesa (Moliere).">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425777724828" ID="ID_57733550" MODIFIED="1425777751651" POSITION="left" TEXT="Contexto Hist&#xf3;rico">
+<icon BUILTIN="idea"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1425779126846" ID="ID_1199572062" MODIFIED="1425779168231" TEXT="s. XVII">
+<icon BUILTIN="idea"/>
+<node CREATED="1425779172566" ID="ID_171875392" MODIFIED="1425779181075" TEXT="Cultural">
+<icon BUILTIN="idea"/>
+<node CREATED="1425779184654" ID="ID_1993904802" MODIFIED="1425779193227" TEXT="Se caracteriza por">
+<icon BUILTIN="idea"/>
+<node CREATED="1425779198861" ID="ID_1285612382" MODIFIED="1425779282046" TEXT="*Crisis religiosa: Reforma y Contrarreforma. *Visi&#xf3;n pesimista de los hechos o de la existencia. *Predominio de las formas retorcidas y artificiosas en el arte. *Temas: cr&#xed;tica de la sociedad, fugacidad de la vida, muerte y desenga&#xf1;o.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+<node CREATED="1425779292656" ID="ID_713414547" MODIFIED="1425779301348" TEXT="Social">
+<icon BUILTIN="idea"/>
+<node CREATED="1425779303743" ID="ID_1757730031" MODIFIED="1425779310205" TEXT="Se produce">
+<icon BUILTIN="idea"/>
+<node CREATED="1425779313221" ID="ID_1165535286" MODIFIED="1425779341842" TEXT="Crisis pol&#xed;tica, econ&#xf3;mica y social. *Inicio de la decadencia del Imperio Espa&#xf1;ol.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
