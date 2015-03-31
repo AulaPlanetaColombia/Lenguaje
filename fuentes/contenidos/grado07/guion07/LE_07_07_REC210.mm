a64b392f5fa0d038de1e4eb4abe02cb337b730a4
Gabriel Rojas Andrade <gabriel0000@gmail.com>
2015-03-31 17:28:29 -0500

    LE_10_01 Revisado con corrección de estilo. LE_07_02 En edición LE_07_07 archivos de autor

---
+++ fuentes/contenidos/grado07/guion07/LE_07_07_REC210.mm
@@ -0,0 +1,95 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1427776422286" ID="ID_755863366" MODIFIED="1427779097665" TEXT="El diccionario">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427776447040" HGAP="12" ID="ID_1555227375" MODIFIED="1427779097665" POSITION="right" TEXT="Se encuentran diferentes tipos de palabras" VSHIFT="89">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427776487860" ID="ID_353929261" MODIFIED="1427779097665" TEXT="Hom&#xf3;nimas">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427776601875" HGAP="8" ID="ID_333602028" MODIFIED="1427779097665" TEXT="Tienen un origen diferente pero se escriben igual" VSHIFT="-67">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427777000257" HGAP="10" ID="ID_1551966893" MODIFIED="1427779097665" TEXT="Pueden ser" VSHIFT="-45">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427777027525" ID="ID_1630259818" MODIFIED="1427779097665" TEXT="Hom&#xf3;fonas">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427777234127" HGAP="16" ID="ID_881107562" MODIFIED="1427779097665" TEXT="Suenan igual pero se escriben diferente" VSHIFT="-85">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1427777039613" ID="ID_1757168655" MODIFIED="1427779097665" TEXT="Hom&#xf3;grafas">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427777216875" HGAP="9" ID="ID_61303150" MODIFIED="1427779097664" TEXT="Se escriben igual pero tienen diferente significado" VSHIFT="-38">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1427776511589" ID="ID_1454932717" MODIFIED="1427779097664" TEXT="Polis&#xe9;micas">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427777779136" HGAP="12" ID="ID_1193661519" MODIFIED="1427779097664" TEXT="Tienen el mismo origen pero diferente significados" VSHIFT="-16">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1427778167212" ID="ID_1129781702" MODIFIED="1427779097664" POSITION="left" TEXT="Tambi&#xe9;n existe la Paronomasia">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427778207055" HGAP="13" ID="ID_1229065280" MODIFIED="1427779097664" TEXT="Agrupaci&#xf3;n de palabras que tienen sonidos similares" VSHIFT="-25">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427779048431" ID="ID_924660689" MODIFIED="1427779097664" TEXT="Juegos de palabras">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+<node CREATED="1427779059591" ID="ID_1452397263" MODIFIED="1427779097664" TEXT="Trabalenguas">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1427778405448" HGAP="-45" ID="ID_1609289243" MODIFIED="1427779097663" POSITION="right" TEXT="Hay diferentes tipos de diccionarios" VSHIFT="76">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427778441089" ID="ID_1624485202" MODIFIED="1427779097663" TEXT="Normativo">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+<node CREATED="1427778450606" ID="ID_1386434774" MODIFIED="1427779097663" TEXT="Enciclop&#xe9;dico">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+<node CREATED="1427778467397" ID="ID_609496644" MODIFIED="1427779097663" TEXT="Bilingue ">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+<node CREATED="1427778747931" ID="ID_248547261" MODIFIED="1427779097663" TEXT="T&#xe9;cnico">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+<node CREATED="1427778759299" ID="ID_1658477654" MODIFIED="1427779097663" TEXT="Sin&#xf3;nimos y ant&#xf3;nimos">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+<node CREATED="1427778777687" ID="ID_701181050" MODIFIED="1427779097663" TEXT="De uso">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1427778799459" HGAP="31" ID="ID_452737492" MODIFIED="1427779097663" POSITION="left" TEXT="Existen otros tipos de consulta de diccionarios como" VSHIFT="111">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427778823976" HGAP="7" ID="ID_611286592" MODIFIED="1427779097663" TEXT="Tecnolog&#xed;as de la informaci&#xf3;n y comunicaciones" VSHIFT="90">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427779107323" HGAP="12" ID="ID_283016382" MODIFIED="1427779145944" TEXT="Internet" VSHIFT="-21">
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1427778912788" HGAP="-69" ID="ID_242929097" MODIFIED="1427779097662" POSITION="right" TEXT="es: " VSHIFT="-130">
+<font NAME="Times New Roman" SIZE="12"/>
+<node CREATED="1427778932606" ID="ID_470588962" MODIFIED="1427779097647" VSHIFT="56">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Times New Roman" size="12.0pt">&#160;una obra <b>impresa</b>&#160;o <b>electr&#243;nica</b>&#160;que recoge de forma ordenada alfab&#233;ticamente las palabras de una lengua para definirlas y aportar informaci&#243;n sobe estas. </font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Times New Roman" SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
