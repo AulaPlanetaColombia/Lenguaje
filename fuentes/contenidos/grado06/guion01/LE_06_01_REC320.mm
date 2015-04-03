b3b8e65688be9ec81288acc5d428c2f67da15ec6
cbpinedat <cristianpinedatriana@gmail.com>
2015-04-02 22:03:33 -0500

    Después de la corrección de estilo
    Archivos con corrección de estilo y posterior revisión

--- fuentes/contenidos/grado06/guion01/LE_06_01_REC320.mm
+++ fuentes/contenidos/grado06/guion01/LE_06_01_REC320.mm
@@ -5,17 +5,17 @@
 <font NAME="Arial" SIZE="28"/>
 <node CREATED="1424736290753" HGAP="0" ID="ID_1026279417" MODIFIED="1424740121836" POSITION="right" STYLE="bubble" TEXT="&#xbf;Qu&#xe9; es?" VSHIFT="13">
 <font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1424736407115" HGAP="16" ID="ID_1583724054" MODIFIED="1424740121836" STYLE="bubble" TEXT="Es el arte de transmitir un mensaje &#xa;ya sea historias reales o de ficci&#xf3;n, &#xa;sentimientos, impresiones o ideas &#xa;con una intenci&#xf3;n est&#xe9;tica. ">
+<node CREATED="1424736407115" HGAP="16" ID="ID_1583724054" MODIFIED="1426596266012" STYLE="bubble" TEXT="Es el arte de transmitir un mensaje &#xa;ya sean historias reales o de ficci&#xf3;n, &#xa;sentimientos, impresiones o ideas, &#xa;con una intenci&#xf3;n est&#xe9;tica. ">
 <font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1424736838722" HGAP="11" ID="ID_505980309" MODIFIED="1424740121836" STYLE="bubble" TEXT="Tambi&#xe9;n puede&#xa;clasificarse en &#xa;g&#xe9;neros." VSHIFT="127">
+<node CREATED="1424736838722" HGAP="11" ID="ID_505980309" MODIFIED="1426596288132" STYLE="bubble" TEXT="Puede&#xa;clasificarse en &#xa;g&#xe9;neros" VSHIFT="127">
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1424738859570" HGAP="15" ID="ID_1177571811" MODIFIED="1424740121852" STYLE="bubble" TEXT="G&#xe9;nero narrativo" VSHIFT="-10">
 <node CREATED="1424739022549" HGAP="15" ID="ID_963946233" MODIFIED="1424740121852" STYLE="bubble" TEXT="Tambi&#xe9;n llamado &#xe9;pico. &#xa;Suele contar historias&#xa;o haza&#xf1;as de uno &#xa;o varios personajes. &#xa;Predomina la prosa. " VSHIFT="1">
 <node CREATED="1424739714000" HGAP="15" ID="ID_493029331" MODIFIED="1424740121852" STYLE="bubble" TEXT="Posee" VSHIFT="-7">
 <node CREATED="1424739741090" ID="ID_545793962" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Narrador."/>
 <node CREATED="1424739741090" ID="ID_1123851845" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Personajes"/>
-<node CREATED="1424739741090" ID="ID_1260193751" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Tiempo y  espacio."/>
-<node CREATED="1424739741090" ID="ID_1896706701" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Escrito en prosa."/>
+<node CREATED="1424739741090" ID="ID_1260193751" MODIFIED="1428028502895" STYLE="bubble" TEXT="&#x2022;        Tiempo y espacio."/>
+<node CREATED="1424739741090" ID="ID_1896706701" MODIFIED="1426596552630" STYLE="bubble" TEXT="&#x2022;        Escritura en prosa."/>
 <node CREATED="1424739741090" ID="ID_726877340" MODIFIED="1424740121852" STYLE="bubble" TEXT="&#x2022;        Estructura dividida en introducci&#xf3;n, nudo y desenlace."/>
 </node>
 </node>
@@ -41,24 +41,24 @@
 </node>
 <node CREATED="1424736811085" HGAP="17" ID="ID_82551639" MODIFIED="1424740121852" STYLE="bubble" TEXT="Posee &#xa;unos&#xa;elementos" VSHIFT="-76">
 <font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1424737246249" HGAP="14" ID="ID_217940880" MODIFIED="1424740121852" STYLE="bubble" TEXT="Caracter&#xed;sticas que clasifican &#xa;la obra seg&#xfa;n  su estructura, &#xa;marca textual, objetividad &#xa;o subjetividad, etc. " VSHIFT="38">
+<node CREATED="1424737246249" HGAP="14" ID="ID_217940880" MODIFIED="1428028462927" STYLE="bubble" TEXT="Caracter&#xed;sticas que clasifican &#xa;la obra seg&#xfa;n su estructura, &#xa;marca textual, objetividad &#xa;o subjetividad, etc. " VSHIFT="38">
 <node CREATED="1424737774559" HGAP="8" ID="ID_1756145112" MODIFIED="1424740121852" STYLE="bubble" TEXT="Modos de expresi&#xf3;n" VSHIFT="39">
 <node CREATED="1424738076059" ID="ID_1672639810" MODIFIED="1424740121852" STYLE="bubble" TEXT="Verso">
 <node CREATED="1424738626711" HGAP="18" ID="ID_1423083344" MODIFIED="1424740121852" STYLE="bubble" TEXT="Conjunto de palabras &#xa;que forman una unidad &#xa;m&#xe9;trica dentro de una &#xa;estrofa po&#xe9;tica.  " VSHIFT="1"/>
 </node>
 <node CREATED="1424738072768" ID="ID_1941002893" MODIFIED="1424740121852" STYLE="bubble" TEXT="Prosa">
-<node CREATED="1424738541265" HGAP="18" ID="ID_63643596" MODIFIED="1426134667573" STYLE="bubble" TEXT="Es la forma &#xa;natural del lenguaje &#xa;para expresar  ideas &#xa;o conceptos." VSHIFT="24"/>
+<node CREATED="1424738541265" HGAP="18" ID="ID_63643596" MODIFIED="1426596342314" STYLE="bubble" TEXT="Es la forma &#xa;natural del lenguaje &#xa;para expresar ideas &#xa;o conceptos." VSHIFT="24"/>
 </node>
 </node>
 <node CREATED="1424737771050" HGAP="14" ID="ID_99170034" MODIFIED="1424740121852" STYLE="bubble" TEXT="Combinaciones textuales" VSHIFT="25">
 <node CREATED="1424738034686" HGAP="19" ID="ID_1384579622" MODIFIED="1424740121852" STYLE="bubble" TEXT="La descripci&#xf3;n" VSHIFT="-17">
-<node CREATED="1424738396601" HGAP="15" ID="ID_1703648611" MODIFIED="1424740121852" STYLE="bubble" TEXT="Realiza representaciones &#xa;ling&#xfc;&#xed;sticas del mundo real &#xa;o ficticio y aquello que &#xa;lo compone." VSHIFT="-10"/>
+<node CREATED="1424738396601" HGAP="15" ID="ID_1703648611" MODIFIED="1426596363303" STYLE="bubble" TEXT="Realiza representaciones &#xa;ling&#xfc;&#xed;sticas del mundo real &#xa;o ficticio y de aquello que &#xa;lo compone." VSHIFT="-10"/>
 </node>
 <node CREATED="1424738024303" HGAP="29" ID="ID_951642262" MODIFIED="1424740121852" STYLE="bubble" TEXT="El di&#xe1;logo" VSHIFT="-10">
-<node CREATED="1424738340287" ID="ID_485227739" MODIFIED="1426134691593" STYLE="bubble" TEXT="Forma del discurso oral y escrito &#xa;en la que se comunican&#xa;entre s&#xed; dos o m&#xe1;s personas&#xa;(interlocutores);&#xa;en un intercambio de ideas"/>
+<node CREATED="1424738340287" ID="ID_485227739" MODIFIED="1426596413924" STYLE="bubble" TEXT="Forma del discurso, oral y escrito, &#xa;en la que se comunican&#xa;entre s&#xed; dos o m&#xe1;s personas&#xa;(interlocutores) en un intercambio de ideas"/>
 </node>
 <node CREATED="1424738004025" HGAP="16" ID="ID_1733776919" MODIFIED="1424740121852" STYLE="bubble" TEXT="La narraci&#xf3;n" VSHIFT="6">
-<node CREATED="1424738139452" ID="ID_1253175934" MODIFIED="1426134703497" STYLE="bubble" TEXT="Tiene el objetivo de exponer hechos, &#xa;sucesos, historias que les suceden&#xa; a unos personajes en un determinado&#xa; tiempo y espacio."/>
+<node CREATED="1424738139452" ID="ID_1253175934" MODIFIED="1426596505208" STYLE="bubble" TEXT="Expone hechos, &#xa;sucesos, historias que les suceden&#xa;a unos personajes en un tiempo y espacio determinados"/>
 </node>
 </node>
 </node>
