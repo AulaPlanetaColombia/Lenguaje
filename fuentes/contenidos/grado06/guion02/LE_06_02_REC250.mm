8433a05cd3bbc3859cf8829fe643bcd6e4285925
cbpinedat <cristianpinedatriana@gmail.com>
2015-04-06 00:29:38 -0500

    Archivos editados para corrección de estilo
    Los archivos están editados, listos para la corrección

--- fuentes/contenidos/grado06/guion02/LE_06_02_REC250.mm
+++ fuentes/contenidos/grado06/guion02/LE_06_02_REC250.mm
@@ -10,8 +10,7 @@
       El g&#233;nero narrativo
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="20"/>
 <node CREATED="1425625827455" ID="ID_1495035569" MODIFIED="1425628438160" POSITION="right" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -23,8 +22,7 @@
       Tambi&#233;n llamado &#233;pico.&#160;&#160;&#160;<br />Su objetivo es contar historias.&#160;&#160;&#160;<br />Predomina la prosa.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425625894082" ID="ID_1682136572" MODIFIED="1425628438160" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -35,20 +33,18 @@
       Posee subg&#233;neros
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1425625962716" HGAP="9" ID="ID_971015682" MODIFIED="1425628438160" STYLE="bubble" VSHIFT="-33">
+</html></richcontent>
+<node CREATED="1425625962716" HGAP="9" ID="ID_971015682" MODIFIED="1428038519134" STYLE="bubble" VSHIFT="-33">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      Los cantares de gesta.
+      Los cantares de gesta
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425626074799" ID="ID_310488948" MODIFIED="1425628438160" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -59,8 +55,7 @@
       Son historias&#160;<br />que cuentan haza&#241;as&#160;<br />realizadas por los&#160;<br />caballeros en la<br />Edad Media.<br />
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1425625990469" HGAP="21" ID="ID_1086187991" MODIFIED="1425628438160" STYLE="bubble" VSHIFT="-42">
@@ -73,8 +68,7 @@
       La novela
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425626134759" HGAP="80" ID="ID_421898549" MODIFIED="1425628438160" STYLE="bubble" VSHIFT="1">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -85,8 +79,7 @@
       Creaciones literarias<br />extensas que narran&#160;<br />sucesos y que&#160;<br />provocan en los&#160;<br />lectores sentimientos&#160;<br />y reflexiones.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1425625999907" HGAP="25" ID="ID_1878838761" MODIFIED="1425628438160" STYLE="bubble" VSHIFT="-52">
@@ -99,8 +92,7 @@
       La f&#225;bula
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425626174240" HGAP="82" ID="ID_55563958" MODIFIED="1425628438160" STYLE="bubble" VSHIFT="-14">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -111,8 +103,7 @@
       Son relatos&#160;<br />protagonizados&#160;<br />por animales que tienen&#160;<br />caracter&#237;sticas humanas<br />y que contienen&#160;<br />una moraleja.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1425626009452" ID="ID_943319434" MODIFIED="1425628438160" STYLE="bubble" VSHIFT="-66">
@@ -125,8 +116,7 @@
       El mito
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425626153562" HGAP="88" ID="ID_651562638" MODIFIED="1425628438160" STYLE="bubble" VSHIFT="-26">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -137,8 +127,7 @@
       Es una narraci&#243;n&#160;<br />de hechos imaginarios&#160;<br />protagonizada&#160;<br />generalmente&#160;<br />por dioses o h&#233;roes,&#160;<br />divulgados de forma oral.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1425626104942" HGAP="63" ID="ID_247180822" MODIFIED="1425628438160" STYLE="bubble" VSHIFT="-17">
@@ -151,8 +140,7 @@
       La epopeya
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425626201132" ID="ID_494966644" MODIFIED="1425628438160" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -163,8 +151,7 @@
       Consiste en la narraci&#243;n&#160;<br />extensa de haza&#241;as&#160;<br />trascendentales&#160;<br />para un pueblo&#160;<br />en torno a la figura&#160;<br />de dioses o&#160;&#160;h&#233;roes.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1425626017500" HGAP="72" ID="ID_1495932851" MODIFIED="1425628438175" STYLE="bubble" VSHIFT="26">
@@ -177,21 +164,20 @@
       La leyenda
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1425626238839" ID="ID_1894043439" MODIFIED="1425628438175" STYLE="bubble">
+</html></richcontent>
+<node CREATED="1425626238839" ID="ID_1894043439" MODIFIED="1428290438341" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      La leyenda, suele&#160;<br />contar un hecho que,&#160;<br />a pesar de ser&#160;<br />fantasioso, incluso&#160;<br />contando hechos&#160;<br />sobrenaturales,&#160;<br />pretende hacerse&#160;<br />pasar por ver&#237;dico.&#160;
+      Suele&#160;<br />contar un hecho que,&#160;<br />a pesar de ser&#160;<br />fantasioso, incluso&#160;<br />contando hechos&#160;<br />sobrenaturales,&#160;<br />pretende hacerse&#160;<br />pasar por ver&#237;dico.&#160;
     </p>
   </body>
 </html>
 </richcontent>
-<node CREATED="1425626813394" HGAP="11" ID="ID_1986488238" MODIFIED="1425628438175" STYLE="bubble" VSHIFT="-43">
+<node CREATED="1425626813394" HGAP="11" ID="ID_1986488238" MODIFIED="1428290470456" STYLE="bubble" VSHIFT="-43">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -201,8 +187,7 @@
       Caracter&#237;sticas
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425626859518" ID="ID_1497810087" MODIFIED="1425628438175" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -213,8 +198,7 @@
       Es un relato popular.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425626859518" ID="ID_1343009790" MODIFIED="1425628438175" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -226,8 +210,7 @@
       De tradici&#243;n oral.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425626859518" ID="ID_332832009" MODIFIED="1425628438175" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -239,8 +222,7 @@
       El autor es desconocido.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425626859518" ID="ID_1259846987" MODIFIED="1425628438175" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -252,8 +234,7 @@
       Se relaciona con un lugar, personaje, o alg&#250;n elemento natural, que hace parte de la realidad.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425626859518" ID="ID_860671429" MODIFIED="1425628438175" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -265,24 +246,23 @@
       Narra&#160;&#160;alguna costumbre o tradici&#243;n de la comunidad a la que pertenece.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
-<node CREATED="1425626859533" ID="ID_192720394" MODIFIED="1425628438175" STYLE="bubble">
+<node CREATED="1425626859533" ID="ID_192720394" MODIFIED="1428290494239" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      Puede intervenir&#160;&#160;seres o animales m&#225;gicos.
+      Pueden intervenir&#160;&#160;seres o animales m&#225;gicos.
     </p>
   </body>
 </html>
 </richcontent>
 </node>
 </node>
-<node CREATED="1425626904176" ID="ID_825431656" MODIFIED="1425628438175" STYLE="bubble">
+<node CREATED="1425626904176" ID="ID_825431656" MODIFIED="1428290497305" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -292,8 +272,7 @@
       Clases&#160;
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425626916090" ID="ID_1769818708" MODIFIED="1425628438175" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -304,8 +283,7 @@
       Leyendas por tem&#225;ticas.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425626954166" ID="ID_804634222" MODIFIED="1425628438175" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -317,8 +295,7 @@
       Leyendas por su lugar de origen.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -333,21 +310,20 @@
       El cuento
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1425626262319" ID="ID_45159638" MODIFIED="1425628438191" STYLE="bubble">
+</html></richcontent>
+<node CREATED="1425626262319" ID="ID_45159638" MODIFIED="1428290461249" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      El cuento es una&#160;<br />narraci&#243;n breve,&#160;<br />basada en hechos&#160;<br />reales o de ficci&#243;n.&#160;
+      Es una&#160;<br />narraci&#243;n breve,&#160;<br />basada en hechos&#160;<br />reales o de ficci&#243;n.&#160;
     </p>
   </body>
 </html>
 </richcontent>
-<node CREATED="1425627033250" HGAP="9" ID="ID_1355300933" MODIFIED="1425628438191" STYLE="bubble" VSHIFT="-76">
+<node CREATED="1425627033250" HGAP="9" ID="ID_1355300933" MODIFIED="1428290500789" STYLE="bubble" VSHIFT="-76">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -357,8 +333,7 @@
       Elementos
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425627051922" ID="ID_1883401985" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -369,8 +344,7 @@
       Tiempo.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627060288" ID="ID_1571748179" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -382,8 +356,7 @@
       Personajes.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627086633" ID="ID_183177265" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -395,8 +368,7 @@
       Espacio.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627097070" ID="ID_1516818966" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -408,8 +380,7 @@
       Estructura.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627121333" ID="ID_433229447" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -421,11 +392,10 @@
       Narrador.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1425627045105" ID="ID_1356873433" MODIFIED="1425628438191" STYLE="bubble">
+<node CREATED="1425627045105" ID="ID_1356873433" MODIFIED="1428290504371" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -435,9 +405,8 @@
       Clases
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1425627252380" ID="ID_751727829" MODIFIED="1425628438191" STYLE="bubble">
+</html></richcontent>
+<node CREATED="1425627252380" ID="ID_751727829" MODIFIED="1428290510919" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -447,8 +416,7 @@
       Cuentos populares o tradicionales.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425627293628" ID="ID_1128318542" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -459,8 +427,7 @@
       Cuentos de hadas.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627313707" ID="ID_1821904722" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -472,8 +439,7 @@
       Cuentos de animales.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627353080" ID="ID_1040382614" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -485,8 +451,7 @@
       Cuentos de costumbres.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1425627284046" ID="ID_1092279477" MODIFIED="1425628438191" STYLE="bubble">
@@ -499,10 +464,9 @@
       Cuentos literarios.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
-<node CREATED="1425627477951" ID="ID_1727194960" MODIFIED="1425628438191" STYLE="bubble">
+<node CREATED="1425627477951" ID="ID_1727194960" MODIFIED="1428290514111" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -512,8 +476,7 @@
       Cuentos por tem&#225;tica.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425627492112" ID="ID_1109864285" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -524,8 +487,7 @@
       Cuentos maravillosos
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627527015" ID="ID_436417259" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -537,8 +499,7 @@
       Cuentos de ciencia ficci&#243;n
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627557745" ID="ID_325407576" MODIFIED="1425628438191" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -550,10 +511,9 @@
       Cuentos realistas.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
-<node CREATED="1425627565176" ID="ID_1781081858" MODIFIED="1425628438191" STYLE="bubble">
+<node CREATED="1425627565176" FOLDED="true" ID="ID_1781081858" MODIFIED="1428290389645" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -563,9 +523,8 @@
       Cuentos de terror
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1425627644599" ID="ID_947112430" MODIFIED="1425628438207" STYLE="bubble">
+</html></richcontent>
+<node CREATED="1425627644599" FOLDED="true" ID="ID_947112430" MODIFIED="1428290389634" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -575,9 +534,8 @@
       Relato que intenta&#160;<br />generar sentimientos&#160;<br />de miedo o terror&#160;<br />en el lector.&#160;
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1425627697110" ID="ID_840286107" MODIFIED="1425628438207" STYLE="bubble">
+</html></richcontent>
+<node CREATED="1425627697110" FOLDED="true" ID="ID_840286107" MODIFIED="1428290389424" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -587,8 +545,7 @@
       Temas
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425627711236" ID="ID_1446646212" MODIFIED="1425628438207" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -599,8 +556,7 @@
       La muerte.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627880955" ID="ID_1563089970" MODIFIED="1425628438207" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -612,8 +568,7 @@
       P&#233;rdida de la integridad f&#237;sica o mental.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627915251" ID="ID_1752457968" MODIFIED="1425628438207" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -625,8 +580,7 @@
       El mal.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627952687" ID="ID_756336107" MODIFIED="1425628438207" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -638,8 +592,7 @@
       El subconsciente y el inconsciente.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425627985525" ID="ID_1683219072" MODIFIED="1425628438207" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -651,13 +604,12 @@
       El propio cuerpo.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
 </node>
-<node CREATED="1425627585985" ID="ID_673558327" MODIFIED="1425628438207" STYLE="bubble">
+<node CREATED="1425627585985" FOLDED="true" ID="ID_673558327" MODIFIED="1428290389646" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -667,9 +619,8 @@
       Cuentos polic&#237;acos.
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1425628031618" ID="ID_1746255945" MODIFIED="1425628438207" STYLE="bubble">
+</html></richcontent>
+<node CREATED="1425628031618" FOLDED="true" ID="ID_1746255945" MODIFIED="1428290389639" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -679,9 +630,8 @@
       Busca solucionar&#160;<br />un misterio planteado.&#160;<br />Regularmente&#160;<br />es un crimen.
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1425628097923" ID="ID_1611759270" MODIFIED="1425628438207" STYLE="bubble">
+</html></richcontent>
+<node CREATED="1425628097923" FOLDED="true" ID="ID_1611759270" MODIFIED="1428290389441" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -691,8 +641,7 @@
       Clases
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1425628105921" ID="ID_1967508620" MODIFIED="1425628438207" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -703,8 +652,7 @@
       Cl&#225;sico.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1425628112817" ID="ID_1676762536" MODIFIED="1425628438207" STYLE="bubble">
 <richcontent TYPE="NODE"><html>
@@ -716,8 +664,7 @@
       Negro.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
