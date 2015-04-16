9d5c27f415ccec25c0e631735a8282774198fe00
Gabriel Rojas Andrade <gabriel0000@gmail.com>
2015-04-16 08:58:27 -0500

    LE_10_04_CO Archivos de autora

---
+++ fuentes/contenidos/grado10/guion04/LE_10_04_REC90.mm
@@ -0,0 +1,57 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1429035986724" ID="ID_1211629780" MODIFIED="1429036092067" TEXT="Los Medios de Comunicaci&#xf3;n">
+<node CREATED="1429036106478" ID="ID_99107593" MODIFIED="1429036387241" POSITION="right" TEXT="Proceso de adquisici&#xf3;n y tratamiento de la informaci&#xf3;n por parte de los usuarios">
+<icon BUILTIN="idea"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1429036393406" ID="ID_1201633965" MODIFIED="1429036399826" TEXT="Se divide en">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036403161" ID="ID_1520430508" MODIFIED="1429036416198" TEXT="adquisici&#xf3;n de la informaci&#xf3;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036443758" ID="ID_136852329" MODIFIED="1429036450034" TEXT="mediante">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036452553" ID="ID_129007951" MODIFIED="1429036476879" TEXT="discursos narrativos, descriptivos y dialogados">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036483533" ID="ID_580969012" MODIFIED="1429036505120" TEXT="*noticia *reportaje *informativo *comunicado en redes sociales *entrevista">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1429036608066" ID="ID_750957588" MODIFIED="1429036619976" TEXT="discursos explicativos">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036624177" ID="ID_883041079" MODIFIED="1429036643075" TEXT="*documental *wiki *web corporativa">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1429036419579" ID="ID_1946631538" MODIFIED="1429036430772" TEXT="divulgaci&#xf3;n de la opini&#xf3;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036529350" ID="ID_1993824014" MODIFIED="1429036531888" TEXT="mediante">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036534575" ID="ID_1737134782" MODIFIED="1429036547446" TEXT="discursos narrativos, descriptivos y dialogados">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036550033" ID="ID_1099345588" MODIFIED="1429036567599" TEXT="*largometraje *cortometraje *serie *entrevista">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1429036197126" ID="ID_760825080" MODIFIED="1429036210125" POSITION="left" TEXT="Medios de comunicaci&#xf3;n de masas">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036214422" ID="ID_1535229239" MODIFIED="1429036222148" TEXT="son">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036225810" ID="ID_1595546362" MODIFIED="1429036251112" TEXT="*Prensa escrita *Radio *Televisi&#xf3;n *Cine *Internet">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036256155" ID="ID_1953187381" MODIFIED="1429036264448" TEXT="se encarga de">
+<icon BUILTIN="idea"/>
+<node CREATED="1429036267218" ID="ID_363989804" MODIFIED="1429036306038" TEXT="Transmitir informaci&#xf3;n, entretener, crear y difundir opini&#xf3;n.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
