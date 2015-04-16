161f5a0627a7915d190d11a85f24c13f9011edb7
cbpinedat <cristianpinedatriana@gmail.com>
2015-04-15 21:33:40 -0500

    Originales Tema 8
    Archivos originales del autor

---
+++ fuentes/contenidos/grado06/guion08/LE_06_08_CO_REC320.mm
@@ -0,0 +1,227 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1428695410592" ID="ID_958619557" MODIFIED="1428695533265" TEXT="LA S&#xcd;LABA ">
+<node CREATED="1428695410592" ID="ID_1524441089" MODIFIED="1428695424277" POSITION="right" TEXT="LA ACENTUACI&#xd3;N ">
+<node CREATED="1428696431661" ID="ID_1336405521" MODIFIED="1428696559735" TEXT="es">
+<icon BUILTIN="idea"/>
+<node CREATED="1428696437417" ID="ID_85215879" MODIFIED="1428696529901" TEXT="la forma adecuada que tiene una palabra para pronunciarse "/>
+</node>
+<node CREATED="1428696547401" HGAP="17" ID="ID_479851203" MODIFIED="1428696659820" TEXT="se divide en" VSHIFT="54">
+<icon BUILTIN="idea"/>
+<node CREATED="1428696571735" ID="ID_1387457768" MODIFIED="1428696582377" TEXT="ACENTO PROS&#xd3;DICO">
+<node CREATED="1428696669175" ID="ID_236786550" MODIFIED="1428697362960" TEXT="es">
+<icon BUILTIN="idea"/>
+<node CREATED="1428697353246" ID="ID_754747316" MODIFIED="1428697410233">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="12.0pt" face="Times,serif">&#160;la mayor fuerza con que pronunciamos una s&#237;laba </font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1428699276746" ID="ID_109876881" MODIFIED="1428699323804" TEXT="Se clasifica ">
+<icon BUILTIN="idea"/>
+<node CREATED="1428697417711" ID="ID_1787043141" MODIFIED="1428699317864" TEXT="seg&#xfa;n su posici&#xf3;n en">
+<node CREATED="1428697475399" ID="ID_208795571" MODIFIED="1428697487472" TEXT="AGUDAS"/>
+<node CREATED="1428697491315" ID="ID_771701847" MODIFIED="1428697499605" TEXT="GRAVES  O LLANAS"/>
+<node CREATED="1428697502782" ID="ID_1836237139" MODIFIED="1428697507773" TEXT="ESDR&#xda;JULAS "/>
+<node CREATED="1428697512849" ID="ID_877069702" MODIFIED="1428697520486" TEXT="SOBREESDR&#xda;JULAS"/>
+</node>
+</node>
+</node>
+<node CREATED="1428696588207" ID="ID_1251444793" MODIFIED="1428699191416" TEXT="ACENTO ORTOGR&#xc1;FICO">
+<node CREATED="1428696683961" ID="ID_1769425698" MODIFIED="1428697602697" TEXT="es">
+<icon BUILTIN="idea"/>
+<node CREATED="1428697623950" ID="ID_47183195" MODIFIED="1428697724882" STYLE="fork" TEXT="SIMBOLO GR&#xc1;FICO O TILDE">
+<node CREATED="1428697552711" ID="ID_1019617883" MODIFIED="1428697800682">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Times,serif">que se le posesiona</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+<node CREATED="1428697676801" ID="ID_1340701494" MODIFIED="1428697713649">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="12.0pt" face="Times,serif">a la vocal t&#243;nica de una palabra</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1428697782752" ID="ID_294818390" MODIFIED="1428697800684" TEXT="segun ">
+<icon BUILTIN="idea"/>
+<node CREATED="1428697816799" ID="ID_148585641" MODIFIED="1428697827932" TEXT="reglas ortogr&#xe1;ficas">
+<node CREATED="1428697858315" ID="ID_1219576113" MODIFIED="1428697933063" TEXT="AGUDAS">
+<node CREATED="1428697968404" ID="ID_1185075127" MODIFIED="1428698184173" TEXT="se acent&#xfa;a si ">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698035298" ID="ID_1234190308" MODIFIED="1428698062360" TEXT="recae el acento en la &#xfa;ltima s&#xed;laba"/>
+<node CREATED="1428698067604" ID="ID_278668034" MODIFIED="1428698093097" TEXT="termina en &quot;n&quot;, &quot;s&quot; o vocal."/>
+</node>
+</node>
+<node CREATED="1428697938227" ID="ID_1877780015" MODIFIED="1428697945239" TEXT="GRAVES O LLANAS">
+<node CREATED="1428697980078" ID="ID_743244917" MODIFIED="1428698184173" TEXT="se acent&#xfa;a si">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698096107" ID="ID_413818918" MODIFIED="1428698109925" TEXT="recae el acento en la pen&#xfa;ltima s&#xed;laba"/>
+<node CREATED="1428698116714" ID="ID_1706883441" MODIFIED="1428698133407" TEXT="no termina en &quot;n&quot;, &quot;s&quot; o vocal."/>
+</node>
+</node>
+<node CREATED="1428697948605" ID="ID_1213423937" MODIFIED="1428697953441" TEXT="ESDR&#xda;JULAS">
+<node CREATED="1428697990582" ID="ID_967943452" MODIFIED="1428698184173" TEXT="se acent&#xfa;a si">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698137962" ID="ID_186349351" MODIFIED="1428698151748" TEXT="recae el acento en la antepen&#xfa;ltima s&#xed;laba"/>
+<node CREATED="1428698204110" ID="ID_1220291674" MODIFIED="1428698218817" TEXT="todas llevan tilde"/>
+</node>
+</node>
+<node CREATED="1428697956863" ID="ID_944366263" MODIFIED="1428698015421" TEXT="SOBREESD&#xda;JULAS">
+<node CREATED="1428698018863" ID="ID_844135729" MODIFIED="1428698184172" TEXT="se acent&#xfa;a si">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698154823" ID="ID_1201034947" MODIFIED="1428698174048" TEXT="recae el acento en la tras - antepen&#xfa;ltima s&#xed;laba"/>
+<node CREATED="1428698222019" ID="ID_1013321098" MODIFIED="1428698228684" TEXT="todas llevan tilde"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1428696605814" ID="ID_1366620645" MODIFIED="1428696612217" TEXT="ACENTO DIACR&#xcd;TICO">
+<node CREATED="1428696690806" ID="ID_720920308" MODIFIED="1428697362960" TEXT="es">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698251590" ID="ID_1667643189" MODIFIED="1428698294836" TEXT="ACENTO ORTOGR&#xc1;FICO">
+<node CREATED="1428698298677" ID="ID_895882505" MODIFIED="1428698362749" TEXT="que">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698333478" ID="ID_1131547549" MODIFIED="1428699163646" TEXT="diferencia las palabras que se escriben con ortograf&#xed;a id&#xe9;ntica ">
+<node CREATED="1428699167204" ID="ID_1536482690" MODIFIED="1428699198724" TEXT="pero">
+<icon BUILTIN="idea"/>
+<node CREATED="1428699175794" ID="ID_179324807" MODIFIED="1428699183724" TEXT="contienen un significado diferente"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1428698376379" HGAP="21" ID="ID_430129050" MODIFIED="1428698421391" TEXT="se encuentra en" VSHIFT="14">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698393478" ID="ID_170230261" MODIFIED="1428698404023" TEXT="MONOS&#xcd;LABOS">
+<node CREATED="1428698448557" ID="ID_1375554638" MODIFIED="1428698467775" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698473171" ID="ID_682092473" MODIFIED="1428698503250" TEXT="&#xe9;l (pronombre)"/>
+<node CREATED="1428698507090" ID="ID_1424318182" MODIFIED="1428698519523" TEXT="t&#xfa; (pronombre)"/>
+<node CREATED="1428698523271" ID="ID_1108736624" MODIFIED="1428698582629" TEXT="t&#xe9; (sustantivo)"/>
+<node CREATED="1428698586352" ID="ID_1348476261" MODIFIED="1428698624655" TEXT="s&#xed; (adverbio afirmativo y pronombre personal)"/>
+<node CREATED="1428698628133" ID="ID_428095998" MODIFIED="1428698643891" TEXT="m&#xed; (pronombre personal)"/>
+<node CREATED="1428698686515" ID="ID_614379622" MODIFIED="1428698720378" TEXT="s&#xe9; (verbo en primera persona del singular)"/>
+<node CREATED="1428698648078" ID="ID_1423462090" MODIFIED="1428698680900" TEXT="m&#xe1;s (adverbio de cantidad, adjetivo, pronombre)"/>
+<node CREATED="1428698725770" ID="ID_433878368" MODIFIED="1428698742267" TEXT="d&#xe9; (forma verbal)"/>
+</node>
+</node>
+<node CREATED="1428698430337" ID="ID_427276236" MODIFIED="1428698440192" TEXT="EXCEPCIONES">
+<node CREATED="1428698459714" ID="ID_323605570" MODIFIED="1428698467775" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698762038" ID="ID_229731259" MODIFIED="1428698788816" TEXT="a&#xfa;n (cuando signifique &quot;todav&#xed;a&quot;)"/>
+</node>
+</node>
+<node CREATED="1428698802979" HGAP="18" ID="ID_1097722888" MODIFIED="1428698833687" TEXT="EXCLAMATIVOS E INTERROGATIVOS " VSHIFT="37">
+<node CREATED="1428698837516" ID="ID_384989518" MODIFIED="1428698845654" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428698848972" ID="ID_1092844375" MODIFIED="1428698967582" TEXT="&#xbf;qu&#xe9;?, &#xa1;qu&#xe9;!"/>
+<node CREATED="1428698974755" ID="ID_1376775240" MODIFIED="1428698996225" TEXT="&#xbf;c&#xf3;mo?, &#xa1;c&#xf3;mo! "/>
+<node CREATED="1428699001519" ID="ID_1016704239" MODIFIED="1428699019057" TEXT="&#xbf;cu&#xe1;ndo?, &#xa1;cu&#xe1;ndo! "/>
+<node CREATED="1428699023052" ID="ID_1166556920" MODIFIED="1428699040280" TEXT="&#xbf;cu&#xe1;nto?, &#xa1;cu&#xe1;nto! "/>
+<node CREATED="1428699044377" ID="ID_1122271914" MODIFIED="1428699059784" TEXT="&#xbf;d&#xf3;nde?, &#xa1;d&#xf3;nde! "/>
+<node CREATED="1428699065761" ID="ID_1283294958" MODIFIED="1428699080572" TEXT="&#xbf;qui&#xe9;n?, &#xa1;qui&#xe9;n! "/>
+<node CREATED="1428699093699" ID="ID_1034502988" MODIFIED="1428699122304" TEXT="&#xbf;cu&#xe1;l?, &#xa1;cu&#xe1;l! "/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1428695546613" ID="ID_120807637" MODIFIED="1428695639231" POSITION="left" TEXT="es">
+<icon BUILTIN="idea"/>
+<node CREATED="1428695578328" ID="ID_815076911" MODIFIED="1428695595374" TEXT="un conjunto de sonidos que se enuncian en un solo golpe de voz">
+<node CREATED="1428695652713" ID="ID_1652381528" MODIFIED="1428695660806" TEXT="se divide en ">
+<icon BUILTIN="idea"/>
+<node CREATED="1428695664396" ID="ID_575274901" MODIFIED="1428695792241" TEXT="&#xc1;TONAS">
+<node CREATED="1428695696125" ID="ID_1845560896" MODIFIED="1428696021810" TEXT="son aquellas">
+<icon BUILTIN="idea"/>
+<node CREATED="1428695707246" ID="ID_101831623" MODIFIED="1428695822386" TEXT="que no reciben ninguna fuerza sonora "/>
+</node>
+</node>
+<node CREATED="1428695676403" ID="ID_1618144184" MODIFIED="1428695795778" TEXT="T&#xd3;NICAS">
+<node CREATED="1428695831689" ID="ID_1726193902" MODIFIED="1428696025199" TEXT="son aquellas">
+<icon BUILTIN="idea"/>
+<node CREATED="1428695847652" ID="ID_1237290052" MODIFIED="1428695983414" TEXT="que producen un golpe de voz m&#xe1;s fuerte que las otras "/>
+</node>
+</node>
+</node>
+<node CREATED="1428696140251" ID="ID_253597540" MODIFIED="1428696156097" TEXT="se clasifican en " VSHIFT="66">
+<icon BUILTIN="idea"/>
+<node CREATED="1428696160876" ID="ID_592722422" MODIFIED="1428696247087" TEXT="MONOS&#xcd;LABAS ">
+<node CREATED="1428696211439" ID="ID_50588450" MODIFIED="1428696372227" TEXT="son">
+<icon BUILTIN="idea"/>
+<node CREATED="1428696253976" ID="ID_333292335" MODIFIED="1428696298737" TEXT="palabras que tienen una sola s&#xed;laba"/>
+</node>
+</node>
+<node CREATED="1428696178075" ID="ID_1622828840" MODIFIED="1428696185448" TEXT="BIS&#xcd;LABAS">
+<node CREATED="1428696220535" ID="ID_456065364" MODIFIED="1428696372227" TEXT="son">
+<icon BUILTIN="idea"/>
+<node CREATED="1428696305153" ID="ID_1819904826" MODIFIED="1428696325891" TEXT="palabras que cuentan con dos s&#xed;labas"/>
+</node>
+</node>
+<node CREATED="1428696191183" ID="ID_712118475" MODIFIED="1428696196445" TEXT="TRIS&#xcd;LABAS">
+<node CREATED="1428696228466" ID="ID_1984960167" MODIFIED="1428696372226" TEXT="son">
+<icon BUILTIN="idea"/>
+<node CREATED="1428696336220" ID="ID_811050016" MODIFIED="1428696336220">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <font size="12.0pt" face="Times,serif"><span style="font-size: 12.0pt; font-family: Times,serif" lang="ES-TRAD">palabras que cuentan con tres s&#237;labas</span></font>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1428696199764" ID="ID_1189062726" MODIFIED="1428696206415" TEXT="POLIS&#xcd;LABAS">
+<node CREATED="1428696236962" ID="ID_1725362761" MODIFIED="1428696372226" TEXT="son">
+<icon BUILTIN="idea"/>
+<node CREATED="1428696349412" ID="ID_1661054056" MODIFIED="1428696358055">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="12.0pt" face="Times,serif">palabras que cuentan con cuatro s&#237;labas o m&#225;s s&#237;labas</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
