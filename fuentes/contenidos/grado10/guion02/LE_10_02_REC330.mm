a952f7db1914b87ab564cfa893e2612719438fe3
kosmosdelfico <kosmosdelfico@gmail.com>
2015-07-31 12:47:17 -0500

    Corrección Escaleta 1 de 10º

---
+++ fuentes/contenidos/grado10/guion02/LE_10_02_REC330.mm
@@ -0,0 +1,104 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1426304254080" ID="ID_89764084" MODIFIED="1426304265200" TEXT="La Literatura de la Ilustraci&#xf3;n">
+<node CREATED="1426304302387" ID="ID_1482958130" MODIFIED="1426304359647" POSITION="right" TEXT="Literatura">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304347421" ID="ID_959694432" MODIFIED="1426304357440" TEXT="Poes&#xed;a">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304393869" ID="ID_1106639666" MODIFIED="1426304400655" TEXT="Se considera">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304435784" ID="ID_1853479295" MODIFIED="1426304447396" TEXT="Una herramienta para instruir">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304453618" ID="ID_976642690" MODIFIED="1426304457755" TEXT="Tipos">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304467038" ID="ID_1582089331" MODIFIED="1426304475448" TEXT="Poes&#xed;a Anacre&#xf3;ntica">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304568612" ID="ID_21246569" MODIFIED="1426304575221" TEXT="Representante">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304577793" ID="ID_43961906" MODIFIED="1426304587975" TEXT="Mel&#xe9;ndez Vald&#xe9;s">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+<node CREATED="1426304478485" ID="ID_1188531207" MODIFIED="1426304488737" TEXT="F&#xe1;bula">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304491955" ID="ID_1617039651" MODIFIED="1426304502179" TEXT="Influencia">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304519248" ID="ID_850352572" MODIFIED="1426304525061" TEXT="La Fontaine">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1426304505165" ID="ID_48426289" MODIFIED="1426304511905" TEXT="Representantes">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304532666" ID="ID_1479363473" MODIFIED="1426304545636" TEXT="*Samaniego *Iiriarte">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426304359649" ID="ID_1846245278" MODIFIED="1426304365717" TEXT="Prosa">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304409336" ID="ID_1371094869" MODIFIED="1426304414321" TEXT="Se destaca">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304597619" ID="ID_954447511" MODIFIED="1426304617042" TEXT="Ensayo did&#xe1;ctico / human&#xed;stico">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304625624" ID="ID_793321369" MODIFIED="1426304629507" TEXT="es">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304637549" ID="ID_108280519" MODIFIED="1426304698248" TEXT="Texto de estructura libre cuya finalidad es reflexionar e incidir en la situaci&#xf3;n del pa&#xed;s, la educaci&#xf3;n, las costumbres, el trabajo, etc.">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304708083" ID="ID_408405830" MODIFIED="1426304713652" TEXT="Representantes">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304716476" ID="ID_210924290" MODIFIED="1426304801714" TEXT="*Feijoo, Teatro Cr&#xed;tico Universal *Jovellanos, Memoria e Informe *Cadalso, Cartas Marruecas">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426304368151" ID="ID_1005088106" MODIFIED="1426304381812" TEXT="Teatro">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304418689" ID="ID_145792724" MODIFIED="1426304427650" TEXT="Se caracteriza por">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304815589" ID="ID_1878682573" MODIFIED="1426304949179" TEXT="*Ser un instrumento para educar a la sociedad. *Cumplir con las tres unidades cl&#xe1;sicas: acci&#xf3;n, tiempo y espacio. *Mostrar el lenguaje propio del personaje al atender su condici&#xf3;n. *Acci&#xf3;n veros&#xed;mil. *No mezclar lo c&#xf3;mico con lo tr&#xe1;gico.">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304952843" ID="ID_387260977" MODIFIED="1426304963801" TEXT="Representante">
+<icon BUILTIN="idea"/>
+<node CREATED="1426304967510" ID="ID_1611902425" MODIFIED="1426305007937" TEXT="Fern&#xe1;ndez de Morat&#xed;n, La Comedia Nueva y El S&#xed; de las Ni&#xf1;as.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426304330806" ID="ID_631281388" MODIFIED="1426304341725" POSITION="left" TEXT="Contexto Hist&#xf3;rico">
+<icon BUILTIN="idea"/>
+<node CREATED="1426305056455" ID="ID_1593905019" MODIFIED="1426305066980" TEXT="s. XVIII">
+<icon BUILTIN="idea"/>
+<node CREATED="1426305070829" ID="ID_1270127415" MODIFIED="1426305078363" TEXT="Cultural">
+<icon BUILTIN="idea"/>
+<node CREATED="1426305091610" ID="ID_774276619" MODIFIED="1426305096075" TEXT="Se caracteriza por">
+<icon BUILTIN="idea"/>
+<node CREATED="1426305100311" ID="ID_680838507" MODIFIED="1426305251931" TEXT="*Influencia francesa (lenguaje, moda, etc.) *Predominio de la raz&#xf3;n. raz&#xf3;n = Luz, es decir, Siglo de las Luces. *Confianza en la ciencia. *B&#xfa;squeda de la verdad, no de la belleza. *Nacimiento de la Enciclopedia e influencia del pensamiento de Voltaire, Montesquieu, Rousseau y Kant. *Nueva est&#xe9;tica: el Neoclasicismo (verosimilitud, normas cl&#xe1;sicas e intenci&#xf3;n did&#xe1;ctica.)">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+<node CREATED="1426305082566" ID="ID_932394648" MODIFIED="1426305087049" TEXT="Social">
+<icon BUILTIN="idea"/>
+<node CREATED="1426305257286" ID="ID_1851920828" MODIFIED="1426305262678" TEXT="Se produce">
+<icon BUILTIN="idea"/>
+<node CREATED="1426305266384" ID="ID_1183765088" MODIFIED="1426305364602" TEXT="*Voluntad de educar (alfabetizaci&#xf3;n) y reformar (econom&#xed;a, sociedad, etc.) *Despotismo ilustrado en pol&#xed;tica. *Defensa de los principios de libertad, igualdad y fraternidad (Revoluci&#xf3;n Francesa).">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
